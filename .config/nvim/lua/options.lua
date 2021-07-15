0f64aeffe9e2f881d2d7e3ae75422a6dc6ec3613
Alexander Christensen <alexanderlhc@disroot.org>
2021-07-15 22:42:20 +0200

    Space > tabs

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -13,6 +13,7 @@ o.listchars      = {
 }
 o.tabstop        = 2     -- tab takes 2 spaces
 o.shiftwidth     = 2     -- spaces used for (auto)indent
+o.expandtab      = true  -- tabs to spaces
 o.autoindent     = true  -- use current line indent
 o.splitright     = true  -- splits go right
 o.mouse          = "nvi" -- mouse in: normal, visual, insert

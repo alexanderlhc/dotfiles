ac0666685aa98a603850f789665088d982c2ddc3
Alexander Christensen <alexanderlhc@disroot.org>
2021-11-20 20:45:08 +0100

    no comments needed!

--- .config/nvim/lua/plugin-configs/treesitter.lua
+++ .config/nvim/lua/plugin-configs/treesitter.lua
@@ -3,13 +3,3 @@ require('nvim-treesitter.configs').setup {
   highlight = {enable = true},
   indent = {enable = true}
 }
--- require'nvim-treesitter.configs'.setup {
---   ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
---   highlight = {
---     enable = true,              -- false will disable the whole extension
---     additional_vim_regex_highlighting = false,
---   },
---   indent = {
---     enable = true
---   }
--- }

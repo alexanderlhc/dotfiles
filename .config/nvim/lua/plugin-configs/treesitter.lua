d05e81556666e9448c4a6c324e393ab9d95a996c
Alexander Lønbæk Hahn Christensen <alexanderlhc@disroot.org>
2022-01-23 19:40:44 +0100

    Ignore ocaml uncompileable

--- .config/nvim/lua/plugin-configs/treesitter.lua
+++ .config/nvim/lua/plugin-configs/treesitter.lua
@@ -1,5 +1,6 @@
 require('nvim-treesitter.configs').setup {
   ensure_installed = "maintained",
+  ignore_install = { "ocaml", "ocaml_interface", "ocamllex" },
   highlight = {enable = true},
   indent = {enable = true},
   -- From: JoosepAlviste/nvim-ts-context-commentstring

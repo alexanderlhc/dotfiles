aae78a042292c27f9aa8abea3634477c4f465310
Alexander Lønbæk Hahn Christensen <alexanderlhc@disroot.org>
2022-01-26 22:03:52 +0100

    System clipboard integration

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -8,3 +8,11 @@ end
 
 vim.g.mapleader = " "
 map('n', '<Leader>w', ':w<CR>',  { noremap = true, silent = true })
+
+map('n', '<C-p>', '"+p', { noremap = true, silent = true })
+map('v', '<C-p>', '"+p', { noremap = true, silent = true })
+map('n', '<C-P>', '"+P', { noremap = true, silent = true })
+map('v', '<C-P>', '"+P', { noremap = true, silent = true })
+map('n', '<C-y>', '"+y', { noremap = true, silent = true })
+map('v', '<C-y>', '"+y', { noremap = true, silent = true })
+map('n', '<C-Y>', '"+y$', { noremap = true, silent = true })

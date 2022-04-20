aff0762f6247aeace3e9e7009be2d776333f7ea6
Alexander Christensen <alexanderlhc@disroot.org>
2022-04-20 16:05:49 +0200

    lua lsp and diagnostics mappings

--- .config/nvim/lua/plugin-configs/lsp.lua
+++ .config/nvim/lua/plugin-configs/lsp.lua
@@ -1,4 +1,11 @@
 local lspconfig = require 'lspconfig'
+
+local opts = { noremap=true, silent=true }
+vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+
 local on_attach = function(_, bufnr)
   local opts = { buffer = bufnr }
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
@@ -26,8 +33,34 @@ capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 -- Enable the following language servers
 local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver' }
 for _, lsp in ipairs(servers) do
+
   lspconfig[lsp].setup {
     on_attach = on_attach,
     capabilities = capabilities,
   }
+
 end
+
+-- source: https://github.com/nvim-lua/kickstart.nvim/blob/d4cf318b5bc1bb29c7d21dcbdbc5eb622bd91705/init.lua#L240
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
+
+lspconfig.sumneko_lua.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+        path = runtime_path,
+      },
+      diagnostics = {
+        globals = { 'vim' },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file('', true),
+      },
+    },
+  },
+}

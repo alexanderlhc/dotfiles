956b11d52fd3ee53975dee9e6672700e772d424a
Alexander Lønbæk Hahn Christensen <alexanderlhc@disroot.org>
2022-05-15 12:29:48 +0200

    Added TODO with docu

--- .config/nvim/lua/functions.lua
+++ .config/nvim/lua/functions.lua
@@ -1,5 +1,7 @@
 local nvim_command = vim.api.nvim_command
 
+-- TODO: Check :help nvim_command() which suggests using: |nvim_cmd()| or |nvim_exec()|
+
 function ToggleBackground()
   nvim_command(':exec &bg=="light"? "set bg=dark" : "set bg=light"')
 end

7e08e308dd03818e61331fc1e6e1f73c58466737
Alexander Lønbæk Hahn Christensen <alexanderlhc@disroot.org>
2022-05-15 12:30:09 +0200

    Date snippet added

--- .config/nvim/lua/snippets/all.lua
+++ .config/nvim/lua/snippets/all.lua
@@ -1,9 +1,13 @@
 local ls = require "luasnip"
 local snippet = ls.s
+local t = ls.t
 
 return {
-  -- basic, don't need to know anything else
-  --    arg 1: string
-  --    arg 2: a node
-  snippet("simple", t "wow, you were right!"),
+  --snippet("simple", t "wow, you were right!"),
+  --snippet("with newline", t({ "wow", "", "you can skip lines" })),
+  snippet({
+    name= "Date YYYY-MM-DD",
+    dscr = "Date formmated as YYYY-MM-DD, as of now",
+    trig = "date"
+  }, t(os.date("%Y-%m-%d")))
 }

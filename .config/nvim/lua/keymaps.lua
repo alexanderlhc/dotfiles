5a0a6083fd116c6795cc9870bff3056842ca4cb4
Alexander Christensen <alexanderlhc@disroot.org>
2022-05-27 22:18:07 +0200

    keymap function extrapolated

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,14 +1,9 @@
-local function map(mode, lhs, rhs, opts)
-    local options = {noremap = true, silent = true}
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
-    end
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
+local map = require('utility').map
 
 vim.g.mapleader = " "
 map('n', '<Leader>w', ':w<CR>',  { noremap = true, silent = true })
 
+-- Clipboard
 map('n', '<Leader>p', '"+p', { noremap = true, silent = true })
 map('v', '<Leader>p', '"+p', { noremap = true, silent = true })
 map('n', '<Leader>P', '"+P', { noremap = true, silent = true })

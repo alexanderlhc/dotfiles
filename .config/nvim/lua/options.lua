db84ec61ca9a19b422033d7ee1736e502a425df8
Alexander Christensen <alexanderlhc@disroot.org>
2022-07-29 21:07:49 +0200

    wider columns and diagnostics

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,6 +1,8 @@
 local o = vim.opt
 
 o.number         = true -- line numbers
+o.signcolumn     = "yes" -- diagnostics in number column
+o.numberwidth    = 5    -- min width for number column (default: 4)
 o.ignorecase     = true -- ignore case
 o.smartcase      = true -- ignore case unless contain mixed
 o.lazyredraw     = true -- disable redraw during macro and cmds (speed)

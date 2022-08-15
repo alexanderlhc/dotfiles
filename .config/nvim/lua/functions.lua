f8ba233dd1a225a1d0c1d66c7c803f0d4179a83b
Alexander Christensen <alexanderlhc@disroot.org>
2022-08-15 08:12:22 +0200

    DiffSinceSave() shows what has changed since previous save

--- .config/nvim/lua/functions.lua
+++ .config/nvim/lua/functions.lua
@@ -2,8 +2,18 @@ local nvim_command = vim.api.nvim_command
 
 -- TODO: Check :help nvim_command() which suggests using: |nvim_cmd()| or |nvim_exec()|
 
+----------------
+-- ToggleBackground
 function ToggleBackground()
-  nvim_command(':exec &bg=="light"? "set bg=dark" : "set bg=light"')
+	nvim_command(':exec &bg=="light"? "set bg=dark" : "set bg=light"')
 end
 
-nvim_command(':command ToggleBackground lua ToggleBackground()')
+nvim_command(":command ToggleBackground lua ToggleBackground()")
+
+----------------
+-- DiffSinceSave
+function DiffSinceSave()
+	nvim_command(":w !diff % -")
+end
+
+nvim_command(":command DiffSinceSave lua DiffSinceSave()")

06dda5737c4cab655155e2c3690ac31840a1b0c8
Alexander Christensen <alexanderlhc@disroot.org>
2022-09-30 22:22:18 +0200

    refactored

--- .config/bspwm/autolaunch.sh
+++ .config/bspwm/autolaunch.sh
@@ -7,5 +7,5 @@ pgrep -x nm-applet > /dev/null || nm-applet &
 pgrep -x picom > /dev/null || picom &
 pgrep -x greenclip > /dev/null || greenclip daemon &
 pgrep -x nextcloud > /dev/null || nextcloud &
-redshift -l "$(curl -s "https://location.services.mozilla.com/v1/geolocate?key=geoclue" | jq '.location.lat, .location.lng' | tr '\n' ':' | sed 's/:$//')" &
-~/.config/polybar/launch.sh &
+pgrep -x redshift > /dev/null || redshift -l "$(curl -s "https://location.services.mozilla.com/v1/geolocate?key=geoclue" | jq '.location.lat, .location.lng' | tr '\n' ':' | sed 's/:$//')" &
+~/.config/polybat/launch.sh &
--- .config/bspwm/monitors.sh
+++ .config/bspwm/monitors.sh
@@ -1,14 +1,13 @@
-#!/bin/env sh
+#!/bin/env bash
 
-/home/alexander/.screenlayout/raskrask.sh & # Monitor layout
-# if [[ "$HOST" -eq "potentilla" ]]
-# then
+if [ "$HOSTNAME" = veronica ]
+then
+  bspc monitor DP-0 -d I II III
+  bspc monitor DP-2 -d 1 2 3 4 5 6 7 8 9
+  hsetroot -cover ~/Nextcloud/wallpapers/IMG_20200502_151338.jpg
+else
+  notify-send "Work work"
 #   bspc monitor DVI-I-1 -d 1 2 3 4 5 6 7 8 9
 #   bspc monitor DVI-D-0 -d I II III
-# else
-  notify-send "Home sweet home"
-  bspc monitor DP-0 -d 1 2 3 4 5 6 7 8 9
-  bspc monitor DP-2 -d I II III
-# fi
+fi
 
-hsetroot -cover ~/Nextcloud/wallpapers/IMG_20200502_151338.jpg

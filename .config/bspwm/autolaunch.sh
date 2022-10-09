90623c7d40282b46465ca2ee2d60b4f0bd5aa5eb
Alexander Christensen <alexanderlhc@disroot.org>
2022-10-09 23:06:55 +0200

    experimenting with rice

--- .config/bspwm/autolaunch.sh
+++ .config/bspwm/autolaunch.sh
@@ -9,3 +9,5 @@ pgrep -x greenclip > /dev/null || greenclip daemon &
 pgrep -x nextcloud > /dev/null || nextcloud &
 pgrep -x redshift > /dev/null || redshift -l "$(curl -s "https://location.services.mozilla.com/v1/geolocate?key=geoclue" | jq '.location.lat, .location.lng' | tr '\n' ':' | sed 's/:$//')" &
 ~/.config/polybar/launch.sh &
+pkill eww ; /usr/bin/eww -c /home/alexander/.config/eww/topster open bar &
+pkill eww_fullscreen.sh ; /home/alexander/.config/bspwm/eww_fullscreen.sh &

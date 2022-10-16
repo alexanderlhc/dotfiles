8978dd30b679108756f3f57768d643153c005687
Alexander Christensen <alexanderlhc@disroot.org>
2022-10-16 22:56:17 +0200

    picom work with nvidia

--- .config/bspwm/autolaunch.sh
+++ .config/bspwm/autolaunch.sh
@@ -4,7 +4,7 @@ notify-send "Autolaunching applications"
 
 pgrep -x sxhkd > /dev/null || sxhkd &
 pgrep -x nm-applet > /dev/null || nm-applet &
-pgrep -x picom > /dev/null || picom &
+pgrep -x picom > /dev/null || picom --experimental-backends --backend glx --xrender-sync-fence &
 pgrep -x greenclip > /dev/null || greenclip daemon &
 pgrep -x nextcloud > /dev/null || nextcloud &
 pgrep -x redshift > /dev/null || redshift -l "$(curl -s "https://location.services.mozilla.com/v1/geolocate?key=geoclue" | jq '.location.lat, .location.lng' | tr '\n' ':' | sed 's/:$//')" &

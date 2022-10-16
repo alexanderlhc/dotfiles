5cc8c8bb6fc71ea53dc8be9b313846859b882ce8
Alexander Christensen <alexanderlhc@disroot.org>
2022-10-16 17:23:33 +0200

    disable polybar + fix eww going to fullscreen

--- .config/bspwm/autolaunch.sh
+++ .config/bspwm/autolaunch.sh
@@ -8,6 +8,6 @@ pgrep -x picom > /dev/null || picom &
 pgrep -x greenclip > /dev/null || greenclip daemon &
 pgrep -x nextcloud > /dev/null || nextcloud &
 pgrep -x redshift > /dev/null || redshift -l "$(curl -s "https://location.services.mozilla.com/v1/geolocate?key=geoclue" | jq '.location.lat, .location.lng' | tr '\n' ':' | sed 's/:$//')" &
-~/.config/polybar/launch.sh &
+# ~/.config/polybar/launch.sh &
 pkill eww ; /usr/bin/eww -c /home/alexander/.config/eww/topster open bar &
-pkill eww_fullscreen.sh ; /home/alexander/.config/bspwm/eww_fullscreen.sh &
+pkill eww_fullscreen.sh ; sh /home/alexander/.config/bspwm/eww_fullscreen.sh &

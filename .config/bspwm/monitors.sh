5a1f443e7f0f572a14c2597893a66af973f1f8fc
Alexander Christensen <alexanderlhc@disroot.org>
2022-10-17 08:34:07 +0200

    work pc monitors

--- .config/bspwm/monitors.sh
+++ .config/bspwm/monitors.sh
@@ -1,13 +1,14 @@
 #!/bin/env bash
 
-if [ "$HOSTNAME" = veronica ]
-then
+if [ "$HOSTNAME" = veronica ]; then
   bspc monitor DP-0 -d I II III
   bspc monitor DP-2 -d 1 2 3 4 5 6 7 8 9
   hsetroot -cover ~/Nextcloud/wallpapers/IMG_20200502_151338.jpg
+elif [ "$HOSTNAME" = lighthouse ]; then
+  bspc monitor HDMI-0 -d I II III
+  bspc monitor DP-3 -d 1 2 3 4 5 6 7 8 9
 else
   notify-send "Work work"
   #   bspc monitor DVI-I-1 -d 1 2 3 4 5 6 7 8 9
   #   bspc monitor DVI-D-0 -d I II III
 fi
-

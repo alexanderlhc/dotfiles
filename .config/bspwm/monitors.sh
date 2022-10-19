8aa92f80608db340aa961301eff231dae1b75f33
Alexander Christensen <alexanderlhc@disroot.org>
2022-10-19 19:07:12 +0200

    change of wallpaper

--- .config/bspwm/monitors.sh
+++ .config/bspwm/monitors.sh
@@ -4,7 +4,8 @@ if [ "$HOSTNAME" = veronica ]
 then
   bspc monitor DP-0 -d I II III
   bspc monitor DP-2 -d 1 2 3 4 5 6 7 8 9
-  hsetroot -cover ~/Nextcloud/wallpapers/IMG_20200502_151338.jpg
+  # hsetroot -cover ~/Nextcloud/wallpapers/IMG_20200502_151338.jpg
+  hsetroot -cover ~/Downloads/taiki-ishikawa-e3aJ6LyYBHs-unsplash.jpg
 else
   notify-send "Work work"
   #   bspc monitor DVI-I-1 -d 1 2 3 4 5 6 7 8 9

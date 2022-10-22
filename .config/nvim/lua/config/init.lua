e5b1466321b7c74f09cb36fabee8796f4fea9891
Alexander Christensen <alexanderlhc@disroot.org>
2022-10-22 22:18:20 +0200

    typesafed

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -1,20 +1,19 @@
 local g = vim.g
-local o = vim.opt
 
-o.number = true -- line numbers
-o.signcolumn = "yes" -- diagnostics in number column
-o.numberwidth = 5 -- min width for number column (default: 4)
-o.ignorecase = true -- ignore case
-o.smartcase = true -- ignore case unless contain mixed
-o.lazyredraw = true -- disable redraw during macro and cmds (speed)
-o.tabstop = 2 -- tab takes 2 spaces
-o.shiftwidth = 2 -- spaces used for (auto)indent
-o.expandtab = true -- tabs to spaces
-o.autoindent = true -- use current line indent
-o.splitright = true -- splits go right
-o.mouse = "nvi" -- mouse in: normal, visual, insert
-o.syntax = "on"
-o.shell = "/bin/bash"
+vim.opt.number = true -- line numbers
+vim.opt.signcolumn = "yes" -- diagnostics in number column
+vim.opt.numberwidth = 5 -- min width for number column (default: 4)
+vim.opt.ignorecase = true -- ignore case
+vim.opt.smartcase = true -- ignore case unless contain mixed
+vim.opt.lazyredraw = true -- disable redraw during macro and cmds (speed)
+vim.opt.tabstop = 2 -- tab takes 2 spaces
+vim.opt.shiftwidth = 2 -- spaces used for (auto)indent
+vim.opt.expandtab = true -- tabs to spaces
+vim.opt.autoindent = true -- use current line indent
+vim.opt.splitright = true -- splits go right
+vim.opt.mouse = "nvi" -- mouse in: normal, visual, insert
+vim.opt.syntax = "on"
+vim.opt.shell = "/bin/bash"
 
 vim.keymap.set("", "<Space>", "<Nop>", { noremap = true, silent = true })
 g.mapleader = " "

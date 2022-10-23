a986c57cb41af031bb0fab4f5b3bdd3d1d53ea42
Alexander Christensen <alexanderlhc@disroot.org>
2022-10-23 16:41:11 +0200

    icons are edited one place

---
+++ .config/nvim/lua/config/icons.lua
@@ -0,0 +1,17 @@
+local icons = {}
+
+icons.lsp = {
+	Error = " ",
+	Warn = " ",
+	Hint = " ",
+	Info = " ",
+}
+
+icons.cmp = {
+	nvim_lsp = "λ LSP",
+	luasnip = "⋗ LS",
+	buffer = "Ω BU",
+	path = "🖫 PA",
+}
+
+return icons

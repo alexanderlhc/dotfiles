818b7ead8e92d692b25d60e33a5577b7c4b7e855
Alexander Christensen <alexanderlhc@disroot.org>
2022-10-23 16:42:38 +0200

    autoformat

--- .config/nvim/lua/plugins/lsp/installers.lua
+++ .config/nvim/lua/plugins/lsp/installers.lua
@@ -1,21 +1,18 @@
 local M = {}
 
 function M.setup(servers, server_options)
-  local lspconfig = require("lspconfig")
+	require("mason").setup()
 
-  require("mason").setup()
+	require("mason-tool-installer").setup({
+		ensure_installed = { "stylua", "shfmt", "shellcheck", "prettierd" },
+		auto_update = false,
+		run_on_start = true,
+	})
 
-  require("mason-tool-installer").setup({
-    ensure_installed = { "stylua", "shfmt", "shellcheck", "prettierd" },
-    auto_update = false,
-    run_on_start = true,
-  })
-
-  require("mason-lspconfig").setup({
-    ensure_installed = vim.tbl_keys(servers),
-    automatic_installation = false,
-  })
-
+	require("mason-lspconfig").setup({
+		ensure_installed = vim.tbl_keys(servers),
+		automatic_installation = false,
+	})
 end
 
 return M

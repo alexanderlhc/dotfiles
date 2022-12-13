98e45daab834d5ad42259959597ef8998f74a0a8
Alexander Christensen <alexanderlhc@disroot.org>
2022-12-13 22:42:47 +0100

    laptop added as monitor

--- .config/bspwm/monitors.sh
+++ .config/bspwm/monitors.sh
@@ -1,12 +1,15 @@
 #!/bin/env bash
 
 if [ "$HOSTNAME" = veronica ]; then
-  bspc monitor DP-0 -d I II III
-  bspc monitor DP-2 -d 1 2 3 4 5 6 7 8 9
-  # hsetroot -cover ~/Nextcloud/wallpapers/IMG_20200502_151338.jpg
-  hsetroot -cover ~/Downloads/taiki-ishikawa-e3aJ6LyYBHs-unsplash.jpg
+	bspc monitor DP-0 -d I II III
+	bspc monitor DP-2 -d 1 2 3 4 5 6 7 8 9
+	# hsetroot -cover ~/Nextcloud/wallpapers/IMG_20200502_151338.jpg
+	hsetroot -cover ~/Downloads/taiki-ishikawa-e3aJ6LyYBHs-unsplash.jpg
 elif [ "$HOSTNAME" = lighthouse ]; then
-  bspc monitor HDMI-0 -d I II III
-  bspc monitor DP-3 -d 1 2 3 4 5 6 7 8 9
-else
+	bspc monitor HDMI-0 -d I II III
+	bspc monitor DP-3 -d 1 2 3 4 5 6 7 8 9
+elif [ "$HOSTNAME" = fatfrogironscrap ]; then
+	notify-send "fatfrogironscrap"
+	bspc monitor eDP-1 -d 1 2 3 4 5 6 7 8 9
+	hsetroot -cover ~/Media/wallpaper.jpg
 fi

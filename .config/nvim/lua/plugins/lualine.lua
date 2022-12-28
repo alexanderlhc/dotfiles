e672c3d034b6d6e6bebb25eb7cea6386f80ec28a
Alexander Christensen <alexanderlhc@disroot.org>
2022-12-28 12:47:07 +0100

    tab and lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -13,11 +13,31 @@ function M.setup()
 		sections = {
 			lualine_a = { "mode" },
 			lualine_b = { "branch", "diff" },
-			lualine_c = { "filename" },
+			lualine_c = {},
 			lualine_x = {},
 			lualine_y = {},
 			lualine_z = {},
 		},
+		tabline = {
+			lualine_a = {
+				{
+					"filename",
+					symbols = {
+						modified = "[ + ]", -- Text to show when the file is modified.
+						readonly = "[ - ]", -- Text to show when the file is non-modifiable or readonly.
+						unnamed = "[NN ]", -- Text to show for unnamed buffers.
+						newfile = "[New]", -- Text to show for new created file before first writting
+					},
+				},
+				"buffers",
+			},
+			lualine_b = { "branch" },
+			lualine_c = {},
+			lualine_x = {},
+			lualine_y = {},
+			lualine_z = {},
+		},
+		extensions = { "nvim-tree" },
 	})
 end
 

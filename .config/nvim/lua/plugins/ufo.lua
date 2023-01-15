2e799f34b93fccc6616bcc3b86daadf72a98ab5e
Alexander Christensen <alexanderlhc@disroot.org>
2023-01-15 22:16:06 +0100

    ufo collapse formatting

--- .config/nvim/lua/plugins/ufo.lua
+++ .config/nvim/lua/plugins/ufo.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 function M.setup()
+	-- https://github.com/kevinhwang91/nvim-ufo/issues/4
 	-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
 	vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 	vim.keymap.set("n", "zM", require("ufo").closeAllFolds)

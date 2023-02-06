178e44a105d4ad8f6dda306f19368e2d94b7eb81
Alexander Christensen <alexanderlhc@disroot.org>
2023-02-06 08:36:10 +0100

    add neogen for generating comments

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -33,4 +33,11 @@ return {
 			require("neo-tree").setup()
 		end,
 	},
+	{
+		"danymat/neogen",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+		config = true,
+		-- Uncomment next line if you want to follow only stable versions
+		-- version = "*"
+	},
 }

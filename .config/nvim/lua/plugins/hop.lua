a437f93787331de8482e46cd2ec2fe6ca5f39187
Alexander Christensen <alexanderlhc@disroot.org>
2023-02-13 22:37:13 +0100

    added hop to navigate horizontally

---
+++ .config/nvim/lua/plugins/hop.lua
@@ -0,0 +1,49 @@
+return {
+	"phaazon/hop.nvim",
+	version = "2.x.x",
+	config = function()
+		require("hop").setup()
+	end,
+	keys = {
+		{
+			"f",
+			function()
+				local directions = require("hop.hint").HintDirection
+				require("hop").hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+			end,
+			desc = "Hop [f]ind forward",
+		},
+		{
+			"F",
+			function()
+				local directions = require("hop.hint").HintDirection
+				require("hop").hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+			end,
+			desc = "Hop [F]ind backward",
+		},
+		{
+			"t",
+			function()
+				local directions = require("hop.hint").HintDirection
+				require("hop").hint_char1({
+					direction = directions.AFTER_CURSOR,
+					current_line_only = true,
+					hint_offset = -1,
+				})
+			end,
+			desc = "Hop [t]ill forward",
+		},
+		{
+			"T",
+			function()
+				local directions = require("hop.hint").HintDirection
+				require("hop").hint_char1({
+					direction = directions.BEFORE_CURSOR,
+					current_line_only = true,
+					hint_offset = 1,
+				})
+			end,
+			desc = "Hop [T]ill backward",
+		},
+	},
+}

649cfbbfd59fd3065b1ab8ecd9c3d31a598e48ff
Alexander Christensen <alexanderlhc@disroot.org>
2023-02-13 22:16:05 +0100

    add dashboards

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,6 +1,8 @@
 return {
-	-- use({ "TimUntersberger/neogit", requires = "nvim-lua/plenary.nvim" })
-
+	{
+		"TimUntersberger/neogit",
+		requires = "nvim-lua/plenary.nvim",
+	},
 	{
 		"lewis6991/gitsigns.nvim",
 	},

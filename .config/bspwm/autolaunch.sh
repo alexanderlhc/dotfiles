6f92e87fba98e34423c25e075e061560c93a15d3
Alexander Christensen <alexanderlhc@disroot.org>
2023-02-19 14:15:37 +0100

    launch nextcloud in background

--- .config/bspwm/autolaunch.sh
+++ .config/bspwm/autolaunch.sh
@@ -6,7 +6,7 @@ pgrep -x sxhkd >/dev/null || sxhkd &
 pgrep -x nm-applet >/dev/null || nm-applet &
 pgrep -x picom >/dev/null || picom --experimental-backends --backend glx --xrender-sync-fence &
 pgrep -x greenclip >/dev/null || greenclip daemon &
-pgrep -x nextcloud >/dev/null || nextcloud &
+pgrep -x nextcloud >/dev/null || nextcloud --background &
 pgrep -x redshift >/dev/null || redshift -l "$(curl -s "https://location.services.mozilla.com/v1/geolocate?key=geoclue" | jq '.location.lat, .location.lng' | tr '\n' ':' | sed 's/:$//')" &
 # ~/.config/polybar/launch.sh &
 pkill eww

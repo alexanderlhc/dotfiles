51912ad8f42622ce25d2494335adc961ce6e5c70
Alexander Christensen <alexanderlhc@disroot.org>
2023-03-13 08:52:52 +0100

    initialize lazygit with rtp to enable spell

--- .config/nvim/lua/config/initialize.lua
+++ .config/nvim/lua/config/initialize.lua
@@ -11,4 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+	performance = {
+		-- fixes error with spell
+		-- download spell files from http://ftp.vim.org/pub/vim/runtime/spell/
+		rtp = {
+			paths = { vim.fn.stdpath("data") .. "/site" },
+		},
+	},
+})

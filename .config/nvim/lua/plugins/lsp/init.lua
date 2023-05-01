364e9dd6e9a2e1e119792a1f81035cb325ab7f82
Alexander Christensen <alexanderlhc@disroot.org>
2023-05-01 09:39:42 +0200

    reduce virtual text clutter

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -28,7 +28,8 @@ return {
 			diagnostics = {
 				underline = true,
 				update_in_insert = false,
-				virtual_text = { spacing = 4, prefix = "●" },
+				virtual_text = false,
+				-- virtual_text = { spacing = 4, prefix = "●" },
 				severity_sort = true,
 			},
 			autoformat = true,

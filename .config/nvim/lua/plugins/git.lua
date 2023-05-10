9585e325fb91bb8a4d97d483f9398135eb154db2
Alexander Christensen <alexanderlhc@disroot.org>
2023-05-10 11:39:45 +0200

    added Git keymaps

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -2,8 +2,76 @@ return {
 	{
 		"TimUntersberger/neogit",
 		requires = "nvim-lua/plenary.nvim",
+		keys = {
+			{
+				"<leader>G",
+				function()
+					require("neogit").open()
+				end,
+				desc = "Neogit Open",
+			},
+			{
+				"<leader>Gs",
+				function()
+					require("neogit").open_split()
+				end,
+				desc = "Neogit Open Split",
+			},
+			{
+				"<leader>Gv",
+				function()
+					require("neogit").open_vsplit()
+				end,
+				desc = "Neogit Open VSplit",
+			},
+			{
+				"<leader>Gc",
+				function()
+					require("neogit").open_popup()
+				end,
+				desc = "Neogit Open Popup",
+			},
+			{
+				"<leader>Gp",
+				function()
+					require("neogit").open_pull_popup()
+				end,
+				desc = "Neogit Open Pull Popup",
+			},
+			{
+				"<leader>Gr",
+				function()
+					require("neogit").open_push_popup()
+				end,
+				desc = "Neogit Open Push Popup",
+			},
+			{
+				"<leader>Gw",
+				function()
+					require("neogit").open_status()
+				end,
+				desc = "Neogit Open Status",
+			},
+			{
+				"<leader>G?",
+				function()
+					require("neogit").open_help()
+				end,
+				desc = "Neogit Open Help",
+			},
+		},
 	},
 	{
 		"lewis6991/gitsigns.nvim",
+		opts = {},
+		keys = {
+			{
+				"<leader>Gb",
+				function()
+					require("gitsigns").toggle_current_line_blame()
+				end,
+				desc = "Git line blame toggle",
+			},
+		},
 	},
 }

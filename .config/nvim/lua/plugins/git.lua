564ba22ed9e52c75f34e6df6994353e56b638681
Alexander Christensen <alexanderlhc@disroot.org>
2023-07-28 11:46:31 +0200

    octo added for github reviews

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,5 +1,16 @@
 return {
 	{
+		"pwntester/octo.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("octo").setup()
+		end,
+	},
+	{
 		"TimUntersberger/neogit",
 		requires = "nvim-lua/plenary.nvim",
 		keys = {

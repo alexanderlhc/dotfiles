cdd1be39a7efbbf22fb6b10d3ee882c3eea40e18
Alexander Christensen <alexanderlhc@disroot.org>
2023-09-11 11:52:27 +0200

    gpt hotkeys

--- .config/nvim/lua/plugins/gpt.lua
+++ .config/nvim/lua/plugins/gpt.lua
@@ -3,6 +3,24 @@ return {
   config = function()
     require("gp").setup({
       openai_api_key = os.getenv("OPENAI_API_KEY"),
+      chat_model = { model = "gpt-4", temperature = 0.7, top_p = 1 },
     })
   end,
+  keys = {
+    {
+      "<leader>cbn",
+      "<Cmd>GpChatNew<CR>",
+      desc = "New Chat"
+    },
+    {
+      "<leader>cbt",
+      "<Cmd>GpChatToggle<CR>",
+      desc = "Toggle Popup Chat"
+    },
+    {
+      "<leader>cbf",
+      "<Cmd>GpChatFinder<CR>",
+      desc = "Chat Finder"
+    }
+  }
 }

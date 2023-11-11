ca13b604cc321b987872e9f6cd6f26c99c52b9d7
Alexander Christensen <alexanderlhc@disroot.org>
2023-11-11 14:28:01 +0100

    added fT to goto neotree but not toggle

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -1,16 +1,17 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	keys = {
-		{ "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
-	},
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-	},
-	opts = {
-		filesystem = {
-			follow_current_file = true,
-		},
-	},
+  "nvim-neo-tree/neo-tree.nvim",
+  keys = {
+    { "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree Toggle" },
+    { "<leader>fT", "<cmd>Neotree<cr>",        desc = "NeoTree Goto" },
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    filesystem = {
+      follow_current_file = true,
+    },
+  },
 }

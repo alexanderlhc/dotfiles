a0c5884a580bd11333ac9bb4b7d833cec5d8711b
Alexander Christensen <alexanderlhc@disroot.org>
2023-11-11 16:56:34 +0100

    noice shows signature when writing out methods

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,18 +1,29 @@
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
-  opts = {
-    lsp = {
-      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true,
+  config = function()
+    require("noice").setup({
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+        signature = {
+          view = 'custom_lsp_popup', -- currently the signature popup are overlapping with CMP without this trick
+        },
       },
-    },
-  },
-  config = function(opts)
-    require("noice").setup(opts)
+      views = {
+        custom_lsp_popup = {
+          view = 'hover',
+          anchor = 'SW',
+          position = {
+            row = 0,
+          },
+        },
+      },
+    })
   end,
   dependencies = {
     "MunifTanjim/nui.nvim",

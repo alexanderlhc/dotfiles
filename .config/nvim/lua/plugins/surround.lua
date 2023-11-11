7037632861870c260d350687700b94c3a01b08fc
Alexander Christensen <alexanderlhc@disroot.org>
2023-11-11 17:52:44 +0100

    surround fixed and replaced with mini.surround

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -1,5 +1,18 @@
 return {
-	"kylechui/nvim-surround",
-	event = "VeryLazy",
-	opts = {}, -- mappings are not applied without
+  'echasnovski/mini.surround',
+  version = false,
+  opts = {
+    mappings = {
+      add = '<leader>sa', -- Add surrounding in Normal and Visual modes
+      delete = '<leader>sd', -- Delete surrounding
+      find = '<leader>sf', -- Find surrounding (to the right)
+      find_left = '<leader>sF', -- Find surrounding (to the left)
+      highlight = '<leader>sh', -- Highlight surrounding
+      replace = '<leader>sr', -- Replace surrounding
+      update_n_lines = '<leader>sn', -- Update `n_lines`
+
+      suffix_last = '<leader>l', -- Suffix to search with "prev" method
+      suffix_next = '<leader>n', -- Suffix to search with "next" method
+    },
+  }
 }

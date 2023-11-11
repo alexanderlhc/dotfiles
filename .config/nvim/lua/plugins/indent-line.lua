de04217702fac565cbe8b51a089be36df9de5955
Alexander Christensen <alexanderlhc@disroot.org>
2023-11-11 18:03:34 +0100

    indentline now highlights the current scope

--- .config/nvim/lua/plugins/indent-line.lua
+++ .config/nvim/lua/plugins/indent-line.lua
@@ -1,26 +1,33 @@
 return {
-	"lukas-reineke/indent-blankline.nvim",
-	main = "ibl",
-	opts = {
-		indent = {
-			char = "│",
-			tab_char = "│",
-		},
-		scope = { enabled = false },
-		exclude = {
-			filetypes = {
-				"help",
-				"alpha",
-				"dashboard",
-				"neo-tree",
-				"Trouble",
-				"trouble",
-				"lazy",
-				"mason",
-				"notify",
-				"toggleterm",
-				"lazyterm",
-			},
-		},
-	},
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  dependencies = {
+    {
+      'echasnovski/mini.indentscope',
+      version = false,
+      opts = {}
+    }
+  },
+  opts = {
+    indent = {
+      char = "│",
+      tab_char = "│",
+    },
+    scope = { enabled = false },
+    exclude = {
+      filetypes = {
+        "help",
+        "alpha",
+        "dashboard",
+        "neo-tree",
+        "Trouble",
+        "trouble",
+        "lazy",
+        "mason",
+        "notify",
+        "toggleterm",
+        "lazyterm",
+      },
+    },
+  },
 }

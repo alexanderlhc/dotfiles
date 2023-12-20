8b44dd1e035b4a8bb1907725092f496ed3772808
Alexander <alexanderlhc@disroot.org>
2023-12-20 23:01:46 +0100

    chatmodels are replaced with agents

--- .config/nvim/lua/plugins/gpt.lua
+++ .config/nvim/lua/plugins/gpt.lua
@@ -3,7 +3,6 @@ return {
   config = function()
     require("gp").setup({
       openai_api_key = os.getenv("OPENAI_API_KEY"),
-      chat_model = { model = "gpt-4", temperature = 0.7, top_p = 1 },
     })
   end,
   keys = {

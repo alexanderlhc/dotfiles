5f4bb774f21d62e1a13068e386ce3ba7d66ace44
Alexander Christensen <alexanderlhc@disroot.org>
2023-12-28 14:34:09 +0100

    lualine lspprogress

---
+++ .config/nvim/lua/plugins/lualine/components/progress.lua
@@ -0,0 +1,8 @@
+vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
+vim.api.nvim_create_autocmd("User", {
+	group = "lualine_augroup",
+	pattern = "LspProgressStatusUpdated",
+	callback = require("lualine").refresh,
+})
+
+return require("lsp-progress").progress
--- .config/nvim/lua/plugins/lualine/init.lua
+++ .config/nvim/lua/plugins/lualine/init.lua
@@ -7,31 +7,39 @@ https://github.com/nvim-lualine/lualine.nvim
 --]]
 
 return {
-  "nvim-lualine/lualine.nvim",
-  event = "VeryLazy",
-  opts = function()
-    local mode = require("plugins.lualine.components.mode")
-    local macro = require("plugins.lualine.components.macro")
-    local filetype = require("plugins.lualine.components.filetype")
-    local filename = require("plugins.lualine.components.filename")
-    local gitdiff = require("plugins.lualine.components.gitdiff")
-    local diagnostics = require("plugins.lualine.components.diagnostics")
+	"nvim-lualine/lualine.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		{
+			"linrongbin16/lsp-progress.nvim",
+			dependencies = { "nvim-tree/nvim-web-devicons" },
+			config = true,
+		},
+	},
+	opts = function()
+		local mode = require("plugins.lualine.components.mode")
+		local macro = require("plugins.lualine.components.macro")
+		local filetype = require("plugins.lualine.components.filetype")
+		local filename = require("plugins.lualine.components.filename")
+		local gitdiff = require("plugins.lualine.components.gitdiff")
+		local diagnostics = require("plugins.lualine.components.diagnostics")
+		local progress = require("plugins.lualine.components.progress")
 
-    return {
-      options = {
-        theme = "auto",
-        globalstatus = true,
-        disabled_filetypes = { statusline = { "dashboard", "alpha", "lazy" } },
-      },
-      sections = {
-        lualine_a = { { mode }, { macro } },
-        lualine_b = { "branch" },
-        lualine_c = { filetype, filename, },
-        lualine_x = {},
-        lualine_y = { diagnostics },
-        lualine_z = { gitdiff },
-      },
-      extensions = { "neo-tree" },
-    }
-  end,
+		return {
+			options = {
+				theme = "auto",
+				globalstatus = true,
+				disabled_filetypes = { statusline = { "dashboard", "alpha", "lazy" } },
+			},
+			sections = {
+				lualine_a = { { mode }, { macro } },
+				lualine_b = { "branch" },
+				lualine_c = { filetype, filename },
+				lualine_x = { progress },
+				lualine_y = { diagnostics },
+				lualine_z = { gitdiff },
+			},
+			extensions = { "neo-tree" },
+		}
+	end,
 }

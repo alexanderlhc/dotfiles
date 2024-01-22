46d6223e834ba917bcf2056cbe046a4134bfb94a
Alexander Christensen <alexanderlhc@disroot.org>
2024-01-22 20:25:58 +0100

    chatnew to, bump, octo rm

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,16 +1,5 @@
 return {
   {
-    "pwntester/octo.nvim",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require("octo").setup()
-    end,
-  },
-  {
     "TimUntersberger/neogit",
     requires = "nvim-lua/plenary.nvim",
     config = true,

3c643e964097026cbcfaa98d8ae95672bb1b9bd2
Alexander Christensen <alexanderlhc@disroot.org>
2024-02-26 20:48:14 +0100

    lazygit with dotgit

--- .config/fish/functions/dotconfig.fish
+++ .config/fish/functions/dotconfig.fish
@@ -1,3 +1,7 @@
 function dotconfig
-    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv
+  if test $argv[1] = "lazy"
+    lazygit --git-dir=$HOME/.dotfiles/ --work-tree=$HOME
+    return
+  end
+  /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv
 end

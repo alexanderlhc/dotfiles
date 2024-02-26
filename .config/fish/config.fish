117650142efdeb874697131891310e3540d2bea8
Alexander Christensen <alexanderlhc@disroot.org>
2024-02-26 20:48:08 +0100

    rate-update-mirrors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,4 +3,5 @@ if status is-interactive
 end
 
 abbr -a 'v' 'nvim'
+abbr -a 'rate-upate-mirror' 'rate-mirrors --allow-root --disable-comments --protocol https arch  | sudo tee /etc/pacman.d/mirrorlist'
 # abbr -a 'docker stop all' 'docker stop (docker ps -aq)'

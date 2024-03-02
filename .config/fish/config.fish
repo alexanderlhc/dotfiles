d6679b8b18348ce04fda79c64db593b53763d299
Alexander Christensen <alexanderlhc@disroot.org>
2024-03-02 14:20:48 +0100

    added zoxide to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,5 @@ end
 abbr -a 'v' 'nvim'
 abbr -a 'rate-upate-mirror' 'rate-mirrors --allow-root --disable-comments --protocol https arch  | sudo tee /etc/pacman.d/mirrorlist'
 # abbr -a 'docker stop all' 'docker stop (docker ps -aq)'
+
+zoxide init fish | source

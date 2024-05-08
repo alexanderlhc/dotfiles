5f9d8f7b9da4f2445420af829cc014963e8ef5f3
Alexander <alexanderlhc@disroot.org>
2024-05-08 08:34:44 +0200

    default wider buffer manager

--- .config/nvim/lua/plugins/buffers.lua
+++ .config/nvim/lua/plugins/buffers.lua
@@ -1,13 +1,16 @@
 return {
-	"j-morano/buffer_manager.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	keys = {
-		{
-			"<leader>fb",
-			function()
-				require("buffer_manager.ui").toggle_quick_menu()
-			end,
-			desc = "Toggle buffer manager",
-		},
-	},
+  "j-morano/buffer_manager.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    width = 0.8,
+  },
+  keys = {
+    {
+      "<leader>fb",
+      function()
+        require("buffer_manager.ui").toggle_quick_menu()
+      end,
+      desc = "Toggle buffer manager",
+    },
+  },
 }

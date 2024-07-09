bbd6d650930928950e60be65f0712c4b2cc0dd13
Alexander Christensen <alexanderlhc@disroot.org>
2024-07-09 17:30:33 +0200

    tmp disabled

--- .config/nvim/lua/plugins/diagnostics.lua
+++ .config/nvim/lua/plugins/diagnostics.lua
@@ -1,8 +1,9 @@
 -- https://www.reddit.com/r/neovim/comments/1drcd9m/rachartiertinyinlinediagnosticnvim_display/?share_id=NzsugbreTIh56ndwUAubo&utm_content=1&utm_medium=android_app&utm_name=androidcss
-return {
-	"rachartier/tiny-inline-diagnostic.nvim",
-	event = "VeryLazy",
-	config = function()
-		require("tiny-inline-diagnostic").setup()
-	end,
-}
+return {}
+-- return {
+-- 	"rachartier/tiny-inline-diagnostic.nvim",
+-- 	event = "VeryLazy",
+-- 	config = function()
+-- 		require("tiny-inline-diagnostic").setup()
+-- 	end,
+-- }

9c3c24d9cc7c9c40970c140e67e1c0d641e0bf1d
Alexander Christensen <alexanderlhc@disroot.org>
2024-07-10 09:59:48 +0200

    docker stop all containers restart and completion added

---
+++ .config/fish/completions/docker.fish
@@ -0,0 +1,2 @@
+complete -c docker -a "stop all" -d "Stop all running containers"
+complete -c docker -a "no-containers-restart" -d "Stop all running containers without restarting"
--- .config/fish/functions/docker.fish
+++ .config/fish/functions/docker.fish
@@ -1,13 +1,18 @@
 function docker
-    switch $argv[1]
-        case 'stop'
-            if test "$argv[2]" = 'all'
-                echo "docker stop (docker ps -aq)"
-                command docker stop (docker ps -aq)
-            else
-                command docker $argv
-            end
-        case '*'
-            command docker $argv
-    end
+  switch $argv[1]
+    case 'stop'
+      if test "$argv[2]" = 'all'
+        echo "docker stop (docker ps -aq)"
+        command docker stop (docker ps -aq)
+      else
+        command docker $argv
+      end
+    case 'no-containers-restart'
+      echo "docker stop (docker ps -aq)"
+      command docker stop (docker ps -aq)
+    case '*'
+      command docker $argv
+  end
 end
+
+# Completions added in completion/docker.fish:

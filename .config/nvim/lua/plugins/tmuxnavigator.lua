ad751d1e9b20afb103af9fe673fdbbc95de84bf7
Alexander Christensen <alexanderlhc@disroot.org>
2024-07-18 13:31:47 +0200

    config deprecated replaced with opts

--- .config/nvim/lua/plugins/tmuxnavigator.lua
+++ .config/nvim/lua/plugins/tmuxnavigator.lua
@@ -1,48 +1,48 @@
 return {
-  "alexghergh/nvim-tmux-navigation",
-  config = {},
-  keys = {
-    {
-      "<C-h>",
-      function()
-        require('nvim-tmux-navigation').NvimTmuxNavigateLeft()
-      end,
-      desc = "Navigate Left",
-    },
-    {
-      "<C-j>",
-      function()
-        require('nvim-tmux-navigation').NvimTmuxNavigateDown()
-      end,
-      desc = "Navigate Down",
-    },
-    {
-      "<C-k>",
-      function()
-        require('nvim-tmux-navigation').NvimTmuxNavigateUp()
-      end,
-      desc = "Navigate Up",
-    },
-    {
-      "<C-l>",
-      function()
-        require('nvim-tmux-navigation').NvimTmuxNavigateRight()
-      end,
-      desc = "Navigate Right",
-    },
-    {
-      "<C-\\>",
-      function()
-        require('nvim-tmux-navigation').NvimTmuxNavigateLastActive()
-      end,
-      desc = "Navigate to Last Active",
-    },
-    {
-      "<C-Space>",
-      function()
-        require('nvim-tmux-navigation').NvimTmuxNavigateNext()
-      end,
-      desc = "Navigate to Next",
-    },
-  }
+	"alexghergh/nvim-tmux-navigation",
+	opts = {},
+	keys = {
+		{
+			"<C-h>",
+			function()
+				require("nvim-tmux-navigation").NvimTmuxNavigateLeft()
+			end,
+			desc = "Navigate Left",
+		},
+		{
+			"<C-j>",
+			function()
+				require("nvim-tmux-navigation").NvimTmuxNavigateDown()
+			end,
+			desc = "Navigate Down",
+		},
+		{
+			"<C-k>",
+			function()
+				require("nvim-tmux-navigation").NvimTmuxNavigateUp()
+			end,
+			desc = "Navigate Up",
+		},
+		{
+			"<C-l>",
+			function()
+				require("nvim-tmux-navigation").NvimTmuxNavigateRight()
+			end,
+			desc = "Navigate Right",
+		},
+		{
+			"<C-\\>",
+			function()
+				require("nvim-tmux-navigation").NvimTmuxNavigateLastActive()
+			end,
+			desc = "Navigate to Last Active",
+		},
+		{
+			"<C-Space>",
+			function()
+				require("nvim-tmux-navigation").NvimTmuxNavigateNext()
+			end,
+			desc = "Navigate to Next",
+		},
+	},
 }

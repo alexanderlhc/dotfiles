c323b005c2f16d18d2a02f33e74a3dd35523424e
Alexander Christensen <alexanderlhc@disroot.org>
2024-11-12 08:55:17 +0100

    new theme

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,12 +1,22 @@
 return {
 	{
-		'projekt0n/github-nvim-theme',
-		name = 'github-theme',
+		"projekt0n/github-nvim-theme",
+		name = "github-theme",
+		enabled = false,
 		lazy = false,
 		priority = 1000,
 		config = function()
-			require('github-theme').setup({})
-			vim.cmd('colorscheme github_light')
+			require("github-theme").setup({})
+			vim.cmd("colorscheme github_light")
 		end,
-	}
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		opts = { variant = "dawn" },
+		config = function(_, opts)
+			require("rose-pine").setup(opts)
+			vim.cmd("colorscheme rose-pine")
+		end,
+	},
 }

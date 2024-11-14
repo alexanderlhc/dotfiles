43a38d00b9927eb9a17a0b003a9fb2ae6af0da9e
Alexander Christensen <alexanderlhc@disroot.org>
2024-11-14 15:57:05 +0100

    refac

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -9,12 +9,14 @@ return {
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
 		"saghen/blink.cmp",
+		"yioneko/nvim-vtsls",
 	},
 	config = function()
 		require("mason").setup()
 		require("mason-lspconfig").setup(mlsp_opts)
 
 		local capabilities = require("blink.cmp").get_lsp_capabilities()
+
 		require("mason-lspconfig").setup_handlers({
 			function(server_name)
 				require("lspconfig")[server_name].setup({
@@ -22,6 +24,36 @@ return {
 				})
 			end,
 			["rust_analyzer"] = function() end,
+			["vtsls"] = function()
+				require("lspconfig.configs").vtsls = require("vtsls").lspconfig
+				require("lspconfig").vtsls.setup({
+					settings = {
+						vtsls = {
+							experimental = {
+								completion = {
+									enableServerSideFuzzyMatch = true,
+									entriesLimit = 10,
+								},
+							},
+						},
+						typescript = {
+							inlayHints = {
+								parameterNames = { enabled = "literals" },
+								parameterTypes = { enabled = true },
+								variableTypes = { enabled = true },
+								propertyDeclarationTypes = { enabled = true },
+								functionLikeReturnTypes = { enabled = true },
+								enumMemberValues = { enabled = true },
+							},
+							tsserver = {
+								maxTsServerMemory = 8192,
+								-- log = "terse",
+							},
+						},
+					},
+					capabilities = capabilities,
+				})
+			end,
 		})
 	end,
 }

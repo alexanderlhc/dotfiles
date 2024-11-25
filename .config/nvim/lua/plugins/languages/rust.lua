1b39bdae87102cbc1ab5dc8158a68c291b218cf1
Alexander Christensen <alexanderlhc@disroot.org>
2024-11-25 11:15:46 +0100

    comments out nvim cmp

--- .config/nvim/lua/plugins/languages/rust.lua
+++ .config/nvim/lua/plugins/languages/rust.lua
@@ -1,23 +1,23 @@
 return {
 	-- Extend auto completion
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			{
-				"Saecki/crates.nvim",
-				event = { "BufRead Cargo.toml" },
-				opts = {
-					completion = {
-						cmp = { enabled = true },
-					},
-				},
-			},
-		},
-		opts = function(_, opts)
-			opts.sources = opts.sources or {}
-			table.insert(opts.sources, { name = "crates" })
-		end,
-	},
+	-- {
+	-- 	"hrsh7th/nvim-cmp",
+	-- 	dependencies = {
+	-- 		{
+	-- 			"Saecki/crates.nvim",
+	-- 			event = { "BufRead Cargo.toml" },
+	-- 			opts = {
+	-- 				completion = {
+	-- 					cmp = { enabled = true },
+	-- 				},
+	-- 			},
+	-- 		},
+	-- 	},
+	-- 	opts = function(_, opts)
+	-- 		opts.sources = opts.sources or {}
+	-- 		table.insert(opts.sources, { name = "crates" })
+	-- 	end,
+	-- },
 
 	{
 		"nvim-treesitter/nvim-treesitter",

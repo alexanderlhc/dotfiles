b9ebf3f754abcfe58a431c888549d84f8c897151
Alexander Christensen <alexanderlhc@disroot.org>
2024-11-25 10:17:36 +0100

    disable pairs

--- .config/nvim/lua/plugins/mini_pairs.lua
+++ .config/nvim/lua/plugins/mini_pairs.lua
@@ -1,24 +1,25 @@
 return {
-	'echasnovski/mini.pairs',
+	"echasnovski/mini.pairs",
 	version = false,
+	enabled = false,
 	event = "VeryLazy",
 	config = function()
-		require('mini.pairs').setup({
+		require("mini.pairs").setup({
 			modes = { insert = true, command = true, terminal = true },
 
 			mappings = {
-				['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\].' },
-				['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\].' },
-				['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\].' },
+				["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
+				["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
+				["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
 
-				[')'] = { action = 'close', pair = '()', neigh_pattern = '[^\\].' },
-				[']'] = { action = 'close', pair = '[]', neigh_pattern = '[^\\].' },
-				['}'] = { action = 'close', pair = '{}', neigh_pattern = '[^\\].' },
+				[")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
+				["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
+				["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
 
-				['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '[^\\].', register = { cr = false } },
-				["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[^%a\\].', register = { cr = false } },
-				['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^\\].', register = { cr = false } },
+				['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
+				["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
+				["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
 			},
 		})
-	end
+	end,
 }

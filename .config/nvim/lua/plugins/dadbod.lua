536b945d6ed9b4764d084d4e4fc7deb32533378f
Alexander Christensen <alexanderlhc@disroot.org>
2024-11-25 10:17:27 +0100

    completion

--- .config/nvim/lua/plugins/dadbod.lua
+++ .config/nvim/lua/plugins/dadbod.lua
@@ -1,17 +1,44 @@
 return {
-	'kristijanhusak/vim-dadbod-ui',
-	dependencies = {
-		{ 'tpope/vim-dadbod',                     lazy = true },
-		{ 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		dependencies = {
+			{ "tpope/vim-dadbod", lazy = true },
+			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
+		},
+		cmd = {
+			"DBUI",
+			"DBUIToggle",
+			"DBUIAddConnection",
+			"DBUIFindBuffer",
+		},
+		init = function()
+			-- Your DBUI configuration
+			vim.g.db_ui_use_nerd_fonts = 1
+
+			-- Disable folds in output
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "dbout",
+				callback = function()
+					vim.wo.foldenable = false
+				end,
+			})
+		end,
 	},
-	cmd = {
-		'DBUI',
-		'DBUIToggle',
-		'DBUIAddConnection',
-		'DBUIFindBuffer',
+	{
+		"saghen/blink.cmp",
+		optional = true,
+		opts = {
+			sources = {
+				completion = {
+					enabled_providers = { "dadbod" },
+				},
+				providers = {
+					dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+				},
+			},
+		},
+		dependencies = {
+			"kristijanhusak/vim-dadbod-completion",
+		},
 	},
-	init = function()
-		-- Your DBUI configuration
-		vim.g.db_ui_use_nerd_fonts = 1
-	end,
 }

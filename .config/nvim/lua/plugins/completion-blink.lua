a7c73f1cf2b1d20ea8e798a8edf3b32d71fa2b5a
Alexander Christensen <alexanderlhc@disroot.org>
2024-11-26 19:54:40 +0100

    blink

--- .config/nvim/lua/plugins/completion-blink.lua
+++ .config/nvim/lua/plugins/completion-blink.lua
@@ -1,15 +1,10 @@
 return {
 	"saghen/blink.cmp",
 	lazy = false, -- lazy loading handled internally
-	-- optional: provides snippets for the snippet source
 	dependencies = "rafamadriz/friendly-snippets",
 
 	-- use a release tag to download pre-built binaries
 	version = "v0.*",
-	-- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-	-- build = 'cargo build --release',
-	-- If you use nix, you can build from source using latest nightly rust with:
-	-- build = 'nix run .#build-plugin',
 
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
@@ -27,16 +22,37 @@ return {
 			-- will be removed in a future release, assuming themes add support
 			use_nvim_cmp_as_default = true,
 		},
+
+		windows = {
+			autocomplete = {
+				-- winblend = vim.o.pumblend, -- transparency
+			},
+			documentation = {
+				auto_show = true,
+			},
+			ghost_text = {
+				enabled = vim.g.ai_cmp,
+			},
+		},
+
+
 		-- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
 		-- adjusts spacing to ensure icons are aligned
 		nerd_font_variant = "mono",
 
 		-- experimental auto-brackets support
-		-- accept = { auto_brackets = { enabled = true } }
+		accept = { auto_brackets = { enabled = true } },
 
 		-- experimental signature help support
-		-- trigger = { signature_help = { enabled = true } }
+		trigger = { signature_help = { enabled = true } },
+
+		sources = {
+			completion = {
+				enabled_providers = { 'lsp', 'path', 'snippets', 'buffer' },
+			},
+		},
 	},
+
 	-- allows extending the enabled_providers array elsewhere in your config
 	-- without having to redefining it
 	opts_extend = { "sources.completion.enabled_providers" },

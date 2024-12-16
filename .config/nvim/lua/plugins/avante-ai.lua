8b4e6f260773982d33a8233847f39a92f6ef09b5
Alexander Christensen <alexanderlhc@disroot.org>
2024-12-16 10:21:34 +0100

    avanteee

--- .config/nvim/lua/plugins/avante-ai.lua
+++ .config/nvim/lua/plugins/avante-ai.lua
@@ -1,24 +1,19 @@
 return {
 	"yetone/avante.nvim",
 	event = "VeryLazy",
-	enabled = false,
 	lazy = false,
 	version = false, -- set this if you want to always pull the latest change
 	opts = {
 		provider = "openai",
-		openai = {
-			model = "gpt-4o-mini",
-		},
 	},
 	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
 	build = "make",
-	-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
 	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
 		"stevearc/dressing.nvim",
 		"nvim-lua/plenary.nvim",
 		"MunifTanjim/nui.nvim",
 		--- The below dependencies are optional,
+		-- "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
 		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
 		"zbirenbaum/copilot.lua", -- for providers='copilot'
 		{
@@ -33,18 +28,18 @@ return {
 					drag_and_drop = {
 						insert_mode = true,
 					},
+					-- required for Windows users
 					use_absolute_path = true,
 				},
 			},
 		},
-		-- {
-		-- 	-- Make sure to set this up properly if you have lazy=true
-		-- 	"MeanderingProgrammer/render-markdown.nvim",
-		-- 	dependencies = { "nvim-treesitter/nvim-treesitter" },
-		-- 	opts = {
-		-- 		file_types = { "markdown", "Avante" },
-		-- 	},
-		-- 	ft = { "markdown", "Avante" },
-		-- },
+		{
+			-- Make sure to set this up properly if you have lazy=true
+			"MeanderingProgrammer/render-markdown.nvim",
+			opts = {
+				file_types = { "markdown", "Avante" },
+			},
+			ft = { "markdown", "Avante" },
+		},
 	},
 }

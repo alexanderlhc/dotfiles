c68efbca207acf2e43abcd7781f14970e187fd35
Alexander Christensen <alexanderlhc@disroot.org>
2025-04-25 11:13:13 +0200

    rustacean shares lsp onattach

--- .config/nvim/lua/plugins/lang/rust.lua
+++ .config/nvim/lua/plugins/lang/rust.lua
@@ -2,4 +2,12 @@ return {
 	"mrcjkb/rustaceanvim",
 	version = "^6",
 	lazy = false,
+	config = function()
+		local on_attach_shared = require("config.lsp").on_attach
+		vim.g.rustaceanvim = {
+			server = {
+				on_attach = on_attach_shared,
+			},
+		}
+	end,
 }

aa85d14ced35c730fd28084028de9adca7d789d0
Alexander Christensen <alexanderlhc@disroot.org>
2025-04-25 11:35:54 +0200

    lsp mappings

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -9,27 +9,45 @@ vim.diagnostic.config({
 local M = {}
 
 M.on_attach = function(client, bufnr)
+	-- settings
+	vim.lsp.inlay_hint.enable(true)
+
+	-- keymap
+	local get_opts = function(desc)
+		return { buffer = bufnr, desc = desc }
+	end
 	local map = vim.keymap.set
-	-- map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename", buffer = bufnr })
-	map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action", buffer = bufnr })
+	map({ "n", "v" }, "<leader>ca", require("fzf-lua").lsp_code_actions, get_opts("Code Action"))
 
 	-- Rename file mapping
 	map("n", "<leader>cR", function()
 		Snacks.rename.rename_file()
-	end, { desc = "Rename File", buffer = bufnr })
+	end, get_opts("[C]ode [R]ename File"))
 
 	-- Rename symbol mapping
-	map("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Rename", buffer = bufnr })
+	map("n", "<leader>cr", vim.lsp.buf.rename, get_opts("[C]ode [r]ename"))
 
 	-- Next Reference mapping
 	map("n", "]]", function()
 		Snacks.words.jump(vim.v.count1)
-	end, { desc = "Next Reference", buffer = bufnr })
+	end, get_opts("Next Reference"))
 
 	-- Previous Reference mapping
 	map("n", "[[", function()
 		Snacks.words.jump(-vim.v.count1)
-	end, { desc = "Prev Reference", buffer = bufnr })
+	end, get_opts("Previous Reference"))
+
+	vim.keymap.set("n", "gd", require("fzf-lua").lsp_definitions, get_opts("[G]oto [D]efinition"))
+	vim.keymap.set("n", "gD", require("fzf-lua").lsp_declarations, get_opts("[G]oto [D]efinition"))
+	vim.keymap.set("n", "grr", require("fzf-lua").lsp_references, get_opts("[G]oto [R]eference"))
+	vim.keymap.set("n", "gri", require("fzf-lua").lsp_implementations, get_opts("[G]oto [I]mplementation"))
+	vim.keymap.set("n", "gs", require("fzf-lua").lsp_document_symbols, get_opts("[D]ocument [S]ymbols"))
+	vim.keymap.set(
+		"n",
+		"<leader>ws",
+		require("telescope.builtin").lsp_workspace_symbols,
+		get_opts("[W]orkspace [S]ymbols")
+	)
 end
 
 return M

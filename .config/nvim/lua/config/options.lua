86f2cf851b5f1d1b5fd3cd60c54e261f2058a134
Alexander Christensen <alexanderlhc@disroot.org>
2025-04-28 11:35:34 +0200

    adds some default configs

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,3 +5,8 @@ opt.number = true -- Show line numbers
 opt.relativenumber = true -- Show relative line numbers
 opt.signcolumn = "yes" -- Always show the sign column
 opt.wrap = false -- Disable text wrapping
+
+opt.cursorline = true -- Enable highlighting of the current line
+opt.list = true -- Show some invisible characters
+opt.expandtab = true -- Use spaces instead of tabs
+opt.tabstop = 2 -- Number of spaces tabs count for

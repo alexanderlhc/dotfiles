3d3b0e49a60d033e0ef959e1fc75f150e7a3307f
Alexander Christensen <alexanderlhc@disroot.org>
2026-01-28 13:10:39 +0100

    treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,14 +2,25 @@ vim.pack.add({
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects", version = "main" },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter-context" }, -- shows context while scrolling
+	{ src = "https://github.com/MeanderingProgrammer/treesitter-modules.nvim" },
 }, { load = true })
 
 -- Disable entire built-in ftplugin mappings to avoid conflicts.
 -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
 vim.g.no_plugin_maps = true
 
-require("nvim-treesitter").setup({
-	highlight = { enable = true },
-	indent = { enable = true },
-	folds = { enable = true },
+require("nvim-treesitter").setup({})
+require("treesitter-modules").setup({
+	highlight = {
+		enable = true,
+	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "grn",
+			scope_incremental = "grc",
+			node_decremental = "grm",
+		},
+	},
 })

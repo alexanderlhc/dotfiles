b97979af9e258197986d6d5354a249c10335fcb9
Alexander Christensen <alexanderlhc@disroot.org>
2026-02-26 17:08:10 +0100

    task toggle

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -2,4 +2,15 @@ vim.pack.add({
 	"https://github.com/tadmccorkle/markdown.nvim",
 })
 
-require("markdown").setup()
+require("markdown").setup({
+	on_attach = function(bufnr)
+		if vim.bo[bufnr].ft ~= "markdown" then
+			return
+		end
+
+		local map = vim.keymap.set
+		local opts = { buffer = bufnr }
+		map("n", "<M-c>", "<Cmd>MDTaskToggle<CR>", opts)
+		map("x", "<M-c>", ":MDTaskToggle<CR>", opts)
+	end,
+})

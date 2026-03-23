eaa0b6845c300ca911c2b5b9f70e5b7c45b3a882
Alexander Christensen <alexanderlhc@disroot.org>
2026-03-23 09:05:52 +0100

    enda

--- .config/nvim/ftplugin/markdown.lua
+++ .config/nvim/ftplugin/markdown.lua
@@ -1,7 +1,7 @@
 -- Enable spell checking for Markdown buffers.
 local opt = vim.opt_local
 opt.spell = true
-opt.spelllang = "en_us"
+opt.spelllang = "en_us,da"
 opt.spellfile = vim.fn.stdpath("config") .. "/spell/en.utf-8.add"
 
 opt.wrap = true
